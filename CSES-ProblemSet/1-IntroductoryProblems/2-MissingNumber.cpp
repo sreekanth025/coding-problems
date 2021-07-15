613565f0c78b8467c7d949e0802cc2ffe1a7b722
sreekanth025 <sreekanth@Sreekanths-MacBook-Air.local>
2021-07-15 22:09:11 +0530

    cses-1.2

--- CSES-ProblemSet/1-IntroductoryProblems/2-MissingNumber.cpp
+++ CSES-ProblemSet/1-IntroductoryProblems/2-MissingNumber.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Problem Link: https://cses.fi/problemset/task/1083/
+
+#define int long long
+
 // O(n) time, O(n) extra space
 void method_1() {
     
@@ -22,7 +26,22 @@ void method_1() {
     }
 }
 
-int main() {
-    method_1();
+void method_2() {
+
+    int n;
+    cin >> n;
+
+    int sum=0, p;
+    for(int i=0; i<n-1; i++) {
+        cin >> p;
+        sum += p;
+    }
+
+    cout << (n*(n+1))/2 - sum << "\n";
+}
+
+int32_t main() {
+    // method_1();
+    method_2();
     return 0;
 }
