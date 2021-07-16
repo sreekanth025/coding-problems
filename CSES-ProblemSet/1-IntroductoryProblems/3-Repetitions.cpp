a4541832a5d8fa1efc9899bce87048a8973fbd05
sreekanth025 <vs25@iitbbs.ac.in>
2021-07-16 22:17:11 +0530

    cses-1.3

--- CSES-ProblemSet/1-IntroductoryProblems/3-Repetitions.cpp
+++ CSES-ProblemSet/1-IntroductoryProblems/3-Repetitions.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Problem Link: https://cses.fi/problemset/task/1069/
+
 #define int long long
 
-void method_1() {
+void method_1(string str) {
 
+    int ans=1;
+
+    int count=1;
+    for(int i=1; i<str.size(); i++) {
+        if(str[i] == str[i-1]) {
+            count++;
+            ans = max(ans, count);
+        } else {
+            count=1;
+        }
+    }
+
+    cout << ans << "\n";
 }
 
 int32_t main() {
@@ -12,5 +27,5 @@ int32_t main() {
     string str;
     cin >> str;
 
-    method_1();
+    method_1(str);
 }
